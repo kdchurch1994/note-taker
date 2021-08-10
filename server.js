21a8c35e80963d6a05f67bdce1bfcd2fac42edaf
kdchurch1994 <kdchurch@bsu.edu>
2021-08-10 03:32:20 -0400

    setup server.js and create htmlroutes

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const express = require('express'); //Allows us to use the Express NPM package
 const PORT = process.env.PORT || 3001; // This line of code sets the PORT variable to either process.env.PORT, which is telling the app to use that port (environment variable set by Heroku) if it is set up, and if not, default to port 80. If ran locally, it will use port 3001
 const app = express(); //(starts) the Express.js server
 
-const apiRoutes = require('./routes/apiRoutes'); //Tells the server to look for the index.js file in this directory. This will tell the application what routes to use
+//const apiRoutes = require('./routes/apiRoutes'); //Tells the server to look for the index.js file in this directory. This will tell the application what routes to use
 const htmlRoutes = require('./routes/htmlRoutes'); //Tells the server to look for the index.js file in this directory. This will tell the application what routes to use
-
-
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public')); // Express middlware that uses the express.static() method. We provide a file path to the location in our application and instruct the server to make these files static resources. 
                                    
-app.use('/api', apiRoutes);
+// app.use('/api', apiRoutes);
 app.use('/', htmlRoutes); //This is our way of telling the server that any time a client navigates to <ourhost>/api, the app will use the router we set up in apiRoutes. 
 //If / is the endpoint, then the router will serve back our HTML routes. 
 
